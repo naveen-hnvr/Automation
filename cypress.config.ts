b225bedf1142a32ca8877d265ede9513b73954a5
Naveen Naik <naveen.naik@bitloka.com>
2023-04-06 10:26:43 +0530

    file added to gitignore-3

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: { 
+    'baseUrl': 'https://prod.campustrack.net/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,32 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-    cy.screenshot()
+describe('Login Test and validate page title', () => {
+
+  before(() => {
+    cy.visit("/")
+  })
+  beforeEach(() => {
+    cy.get('input[placeholder="Email"]').type(Cypress.env('USERNAME'))
+    cy.get('#password').type(Cypress.env('PASSWORD'))
+    cy.get('input[type="submit"]').click()
+  })
+
+  it('Logs into the application', () => {
+    cy.title().should('eq', 'CampusTrack')
+    cy.url().should('include', '/home')
+  })
+
+  afterEach(() => {
+    cy.contains(Cypress.env('USERNAME')).click()
+    cy.get('i.fa.fa-sign-out').click();
+  })
+
+})
+describe('Login with incorrect credential', () => {
+  it('When user try to login with incorret credential,proper error message should display', () => {
+    cy.visit("/")
+    cy.get('input[placeholder="Email"]').type(Cypress.env('USERNAME'))
+    cy.get('#password').type(Cypress.env('INCORRECT'))
+    cy.get('input[type="submit"]').click()
+    cy.url().should('include', '/home')
+    cy.contains('Invalid Email or Password').should('be.visible')
   })
 })
