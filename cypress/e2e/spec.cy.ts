e3bcac478fadf8d6784fa20649893d6de302d0a4
Naveen Naik <naveen.naik@bitloka.com>
2023-04-06 10:34:50 +0530

    new testcase added

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -29,4 +29,15 @@ describe('Login with incorrect credential', () => {
     cy.url().should('include', '/home')
     cy.contains('Invalid Email or Password').should('be.visible')
   })
+
+})
+  describe('Login with incorrect credential', () => {
+    it('When user try to login with incorret credential,proper error message should display', () => {
+      cy.visit("/")
+      cy.get('input[placeholder="Email"]').type(Cypress.env('USERNAME'))
+      cy.get('#password').type(Cypress.env('INCORRECT'))
+      cy.get('input[type="submit"]').click()
+      cy.url().should('include', '/home')
+      cy.contains('Invalid Email or Password').should('be.visible')
+    })
 })
